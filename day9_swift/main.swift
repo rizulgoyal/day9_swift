650349d3fef905afb3b7ae6568ddd9d0d847142d
rizul <rizulgoyal23@gmail.com>
2019-10-21 18:04:58 -0400

    Initial Commit

---
+++ day9_swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  day9_swift
+//
+//  Created by MacStudent on 2019-10-21.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
